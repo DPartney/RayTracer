1ef5e30255baf222160b7395590ea7c0ccf06f4e
DPartney <dpartney@student.neumont.edu>
2023-12-06 12:33:29 -0700

    Mesh, Scene and Cornell started

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -6,37 +6,46 @@
 #include "Scene.h"
 #include "Material.h"
 #include "Sphere.h"
+#include "Plane.h"
+#include "Triangle.h"
+#include "Mesh.h"
+
+void InitScene01(Scene& scene, const Canvas& canvas);
+void InitScene02(Scene& scene, const Canvas& canvas);
+void InitScene03(Scene& scene, const Canvas& canvas);
 
 int main(int, char**)
 {
 	std::cout << "Hello World";
 
+	const int width = 800;
+	const int height = 600;
+	const int samples = 20;
+	const int depth = 5;
+
 	Renderer renderer;
 	renderer.Initialize();
-	renderer.CreateWindow("main", 800, 600);
-	Canvas canvas(400, 300, renderer);
+	renderer.CreateWindow("main", width, height);
+	Canvas canvas(width, height, renderer);
 
 	seedRandom((unsigned int)time(NULL));
 
 	float aspectRatio = canvas.GetSize().x / (float)canvas.GetSize().y;
-	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 0, 3 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 70.0f, aspectRatio);
+	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 1, 10 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 20.0f, aspectRatio);
 
-	Scene scene = { 20 }; // sky color could be set with the top and bottom color
+	Scene scene(glm::vec3{ 1.0f }, glm::vec3{ 0.5f, 0.7f, 1.0f });
 	scene.SetCamera(camera);
 
 	auto lambertian = std::make_shared<Lambertian>(color3_t{ 0, 0, 1 });
 	auto metal = std::make_shared<Metal>(color3_t{ 1, 1, 1 }, 0.0f);
 
-	for (auto i = 0; i < 10; i++)
-	{
-		std::shared_ptr<Material> material = (random(0, 2) == 1) ? std::dynamic_pointer_cast<Material>(lambertian) : std::dynamic_pointer_cast<Material>(metal);
-		auto sphere = std::make_unique<Sphere>(glm::vec3{ random(-2, 2), random(-2, 2), random(-2, 2) }, 0.1f, material);
-		scene.AddObject(std::move(sphere));
-	}
+	std::shared_ptr<Material> material;
+	
+	InitScene02(scene, canvas);
 
 	// Render Scene
 	canvas.Clear({ 0, 0, 0, 1 });
-	scene.Render(canvas, 10);
+	scene.Render(canvas, samples, depth);
 	canvas.Update();
 
 	bool quit = false;
@@ -76,4 +85,73 @@ int main(int, char**)
 	renderer.Shutdown();
 
 	return 0;
+}
+
+void InitScene01(Scene& scene, const Canvas& canvas)
+{
+	float aspectRatio = canvas.GetSize().x / canvas.GetSize().y;
+	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 2, 10 }, glm::vec3{ 0, 1, 0 }, glm::vec3{ 0, 1, 0 }, 20.0f, aspectRatio);
+	scene.SetCamera(camera);
+
+	// create objects -> add to scene
+	for (int x = -10; x < 10; x++)
+	{
+		for (int z = -10; z < 10; z++)
+		{
+			std::shared_ptr<Material> material;
+
+			// create random material
+			float r = random01();
+			if (r < 0.3f)		material = std::make_shared<Lambertian>(glm::rgbColor(glm::vec3{ random(0, 360), 1.0f, 1.0f }));
+			else if (r < 0.6f)	material = std::make_shared<Metal>(color3_t{ random(0.5f, 1.0f) }, random(0, 0.5f));
+			else if (r < 0.9f)	material = std::make_shared<Dielectric>(color3_t{ 1.0f }, random(1.1f, 2));
+			else				material = std::make_shared<Emissive>(glm::rgbColor(glm::vec3{ random(0, 360), 1.0f, 1.0f }), 5.0f);
+
+			// set random sphere radius
+			float radius = random(0.2f, 0.3f);
+			// create sphere using random radius and material
+			auto sphere = std::make_unique<Sphere>(glm::vec3{ x + random(-0.5f, 0.5f), radius, z + random(-0.5f, 0.5f) }, radius, material);
+			// add sphere to the scene
+			scene.AddObject(std::move(sphere));
+		}
+	}
+
+	auto plane = std::make_unique<Plane>(glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, std::make_shared<Lambertian>(color3_t{ 0.2f }));
+	scene.AddObject(std::move(plane));
+}
+
+void InitScene02(Scene& scene, const Canvas& canvas)
+{
+	float aspectRatio = canvas.GetSize().x / canvas.GetSize().y;
+	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 2, 10 }, glm::vec3{ 0, 1, 0 }, glm::vec3{ 0, 1, 0 }, 20.0f, aspectRatio);
+	scene.SetCamera(camera);
+
+	auto plane = std::make_unique<Plane>(glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, std::make_shared<Lambertian>(color3_t{ 0.2f }));
+	scene.AddObject(std::move(plane));
+
+	auto mesh = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 0, 0, 1 }));
+	mesh->Load("Models/cube.obj", glm::vec3{ 0, 0.5f, 0 }, glm::vec3{ 0, 45, 0 });
+	scene.AddObject(std::move(mesh));
+}
+
+void InitScene03(Scene& scene, const Canvas& canvas) {
+	float aspectRatio = canvas.GetSize().x / (float)canvas.GetSize().y;
+	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3(0, 2, 10), glm::vec3(0, 1, 0), glm::vec3(0, 1, 0), 20.0f, aspectRatio);
+	scene.SetCamera(camera);
+
+	auto white = std::make_shared<Lambertian>(color3_t(1));
+	auto red = std::make_shared<Lambertian>(color3_t(1, 0, 0));
+	auto green = std::make_shared<Lambertian>(color3_t(0, 1, 0));
+
+	auto backWall = std::make_unique<Mesh>(white);
+	backWall->Load("Models/quad.obj", glm::vec3(0), glm::vec3(90, 0, 0));
+	scene.AddObject(std::move(backWall));
+
+	auto floor = std::make_unique<Mesh>(red);
+	floor->Load("Models/quad.obj");
+	scene.AddObject(std::move(floor));
+
+	auto ceiling = std::make_unique<Mesh>(green);
+	ceiling->Load("Models/quad.obj");
+	scene.AddObject(std::move(ceiling));
 }
--- Source/Scene.cpp
+++ Source/Scene.cpp
@@ -2,8 +2,13 @@
 #include "Canvas.h"
 #include "MathUtils.h"
 #include "Random.h"
+#include "Sphere.h"
+#include "Plane.h"
+#include "Mesh.h"
+#include <iostream>
+#include <iomanip>
 
-void Scene::Render(Canvas& canvas, int numSamples)
+void Scene::Render(Canvas& canvas, int numSamples, int depth)
 {
 	// cast ray for each point (pixel) on the canvas
 	for (int y = 0; y < canvas.GetSize().y; y++)
@@ -31,8 +36,10 @@ void Scene::Render(Canvas& canvas, int numSamples)
 				// cast ray into scene
 				// add color value from trace
 				raycastHit_t raycastHit;
-				color += Trace(ray, 0, 100, raycastHit, m_depth);
+				color += Trace(ray, 0, 100, raycastHit, depth);
 			}
+
+			std::cout << std::setprecision(2) << std::setw(5) << ((y / (float)canvas.GetSize().y) * 100) << "%\n";
 
 			// draw color to canvas point (pixel)
 			// get average color (average = (color + color + color) / number of samples)
@@ -41,6 +48,7 @@ void Scene::Render(Canvas& canvas, int numSamples)
 		}
 	}
 }
+
 color3_t Scene::Trace(const ray_t& ray, float minDistance, float maxDistance, raycastHit_t& raycastHit, int depth)
 {
 	bool rayHit = false;
@@ -72,8 +80,8 @@ color3_t Scene::Trace(const ray_t& ray, float minDistance, float maxDistance, ra
 		}
 		else
 		{
-			// reached maximum depth of bounces (color is black)
-			return color3_t{ 0, 0, 0 };
+			// reached maximum depth of bounces (get emissive color, will be black except for Emissive materials)			
+			return raycastHit.material->GetEmissive();
 		}
 	}
 
@@ -84,3 +92,4 @@ color3_t Scene::Trace(const ray_t& ray, float minDistance, float maxDistance, ra
 
 	return color;
 }
+
